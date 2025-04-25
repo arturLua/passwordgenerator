b23a1850666c0ee0275a93e0457becdf4f7ffbba
Artur <pkllipsendereco@gmail.com>
2025-04-25 04:02:23 -0300

    Update GeradorSenha.py
    Reorganized the code, made a bit cleaner and removed unnecessary comments.

--- GeradorSenha.py
+++ GeradorSenha.py
@@ -1,29 +1,20 @@
-# Project Name
-print('[WIP] Password Generator 0.2!\n')
-
-# Importing functions
 import string
 import random
+print('Password Generator\n')
 
-# Characters that will be used in the password
 characterList = list(string.ascii_letters + string.digits + string.punctuation)
 
-def generate_password(length):
-    # Makes sure the password has at least one of each type.
+def generate_password(length): # Generated password will have one of each type, randomize it(shuffle) and put it all together(join).
     password = [
         random.choice(string.ascii_uppercase),
         random.choice(string.ascii_lowercase),
         random.choice(string.digits),
         random.choice(string.punctuation)
     ]
-
     password += random.choices(characterList, k=length-4)
-
-    # Ensure randomness in the password
     random.shuffle(password)
     return ''.join(password)
 
-# Defining password length
 length = None
 while length is None or length < 8:
     print('Choose the length of your password (8+ characters)')
@@ -34,10 +25,6 @@ while length is None or length < 8:
     except ValueError:
         print('Invalid input. Please enter an integer numeric value')
 
-
-# Generating password results
 password = generate_password(length)
 print(f'Your generated password is: {password}')
-
-input("\nPress Enter to exit the CMD")
-
+input("\nPress Enter to exit . . . ")
